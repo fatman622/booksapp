53b41d615f9fa7780ea56eff22625d348e00fced
fatman622 <olegbabiy.ob@gmail.com>
2017-02-13 12:15:43 +0200

    create test with for book

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
+
   # test "the truth" do
   #   assert true
   # end
 
-
 test "should not save book without name" do
   book = Book.new
-  assert_not book.save
+  assert_not book.save, "Saved the article without a title"
 end
+
 end
